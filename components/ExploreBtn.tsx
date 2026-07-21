7964bbf6c5164070b27c4aa93cdc4b2b52707117
Nashte <ANashte@globalindustrial.com>
2026-07-21 19:17:56 +0530

    DataBase implementation

---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { log } from 'console';
+import Image from 'next/image';
+
+
+const ExploreBtn = () => {
+    return (
+        <>
+            <button type='button' id='explore-btn' className='mt-7 mx-auto' onClick={() => console.log('Click')}>
+                <a href="#events">
+                    Explore Events
+                </a>
+            </button>
+        </>
+    )
+}
+
+export default ExploreBtn
